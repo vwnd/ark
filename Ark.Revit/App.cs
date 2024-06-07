b1f77baf35b03d78b108e04ad3e868e88408eeb5
Victor Wanderley Barbosa <vwb@shl.dk>
2024-06-07 14:59:50 +0200

    feat: revit addin project setup

---
+++ Ark.Revit/App.cs
@@ -0,0 +1,21 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+
+namespace Ark.Revit
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class App : IExternalDBApplication
+    {
+        public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
+        {
+            return ExternalDBApplicationResult.Succeeded;
+        }
+
+        public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+        {
+            return ExternalDBApplicationResult.Succeeded;
+        }
+    }
+}
