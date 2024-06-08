a73de0e722446983e6a9bf2b083522ce0398d945
Victor Wanderley Barbosa <vwb@shl.dk>
2024-06-08 11:55:51 +0200

    chore: build

--- Ark.Revit/App.cs
+++ Ark.Revit/App.cs
@@ -1,6 +1,8 @@
 ﻿using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using DesignAutomationFramework;
 
 namespace Ark.Revit
 {
@@ -8,14 +10,38 @@ namespace Ark.Revit
     [Regeneration(RegenerationOption.Manual)]
     public class App : IExternalDBApplication
     {
+
+        public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+        {
+            #if DEBUG
+            application.ApplicationInitialized += HandleApplicationInitializedEvent;
+            return ExternalDBApplicationResult.Succeeded;
+            #endif
+
+            DesignAutomationBridge.DesignAutomationReadyEvent -= HandleDesignAutomationReadyEvent;
+            return ExternalDBApplicationResult.Succeeded;
+        }
+
+        private void HandleApplicationInitializedEvent(object sender, ApplicationInitializedEventArgs e)
+        {
+            Application app = sender as Application;
+            String filePath = @"C:\\Users\\barbosav\\Downloads\\Structurall_detached.rvt\";
+            var document = app.OpenDocumentFile(@"C:\\Users\\barbosav\\Downloads\\Structurall_detached.rvt");
+
+            if (document == null) throw new InvalidOperationException("Could not open document.");
+
+            var data = new TestDesignAutomationData(filePath, document, app);
+            Core.Main(data);
+        }
+
         public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
         {
             return ExternalDBApplicationResult.Succeeded;
         }
 
-        public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+        private void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
         {
-            return ExternalDBApplicationResult.Succeeded;
+            Core.Main(e.DesignAutomationData as IDesignAutomationData);
         }
     }
 }
---
+++ Ark.Revit/Core.cs
@@ -0,0 +1,75 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using DesignAutomationFramework;
+using Objects.BuiltElements;
+using Objects.Converter.Revit;
+using Objects.Geometry;
+using Speckle.Core.Models;
+using System.Diagnostics;
+
+namespace Ark.Revit
+{
+    public interface IDesignAutomationData
+    {
+        public Application RevitApp { get; set; }
+        public Document RevitDoc { get; set; }
+        public string FilePath { get; set; }
+    }
+
+    public class TestDesignAutomationData: IDesignAutomationData
+    {
+        public Application RevitApp { get; set; }
+        public Document RevitDoc { get; set; }
+        public string FilePath { get; set; }
+
+        public TestDesignAutomationData(string filePath, Document revitDoc, Application revitApp)
+        {
+            FilePath = filePath;
+            RevitDoc = revitDoc;
+            RevitApp = revitApp;
+        }
+    }
+
+    public class Core
+    {
+        public static void Main(IDesignAutomationData data)
+        {
+            if (data == null) throw new ArgumentNullException(nameof(data));
+
+            Application rvtApp = data.RevitApp;
+            if (rvtApp == null) throw new InvalidDataException(nameof(rvtApp));
+
+            string modelPath = data.FilePath;
+            if (String.IsNullOrWhiteSpace(modelPath)) throw new InvalidDataException(nameof(modelPath));
+
+            Document doc = data.RevitDoc;
+            if (doc == null) throw new InvalidOperationException("Could not open document.");
+
+            var collector = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StructuralColumns);
+            var columns = collector.ToElements();
+            
+            var converter = new ConverterRevit();
+            converter.SetContextDocument(doc);
+
+            //var speckleObjects = converter.ConvertToSpeckle(columns[0]);
+            var speckleObjects = MockObjects();
+
+            Debug.WriteLine($"Converted {columns.Count} columns to {speckleObjects.GetTotalChildrenCount()} Speckle objects.");
+        }
+
+        private static Base MockObjects()
+        {
+            var data = new Base();
+            var elements = new List<Base>();
+            for (int i = 0; i < 10; i++)
+            {
+                var baseLine = new Objects.Geometry.Line(new Objects.Geometry.Point(i, 0, 0), new Objects.Geometry.Point(i, 0, 3));
+                var column = new Column(baseLine);
+                elements.Add(column);
+            }
+
+            data["elements"] = elements;
+            return data;
+        }
+    }
+}
