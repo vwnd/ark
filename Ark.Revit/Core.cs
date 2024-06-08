fd6208e3289f7796fade447534bfa1be43ae7dca
Victor Wanderley Barbosa <vwb@shl.dk>
2024-06-08 19:58:13 +0200

    feat: commit to speckle

--- Ark.Revit/Core.cs
+++ Ark.Revit/Core.cs
@@ -1,9 +1,15 @@
 ﻿using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.DB;
+using ConverterRevitShared;
 using DesignAutomationFramework;
 using Objects.BuiltElements;
 using Objects.Converter.Revit;
+using Speckle.Core.Api;
+using Speckle.Core.Credentials;
+using Speckle.Core.Helpers;
+using Speckle.Core.Logging;
 using Speckle.Core.Models;
+using Speckle.Core.Transports;
 
 namespace Ark.Revit
 {
@@ -14,7 +20,7 @@ namespace Ark.Revit
         public string FilePath { get; set; }
     }
 
-    public class TestDesignAutomationData: IDesignAutomationData
+    public class TestDesignAutomationData : IDesignAutomationData
     {
         public Application RevitApp { get; set; }
         public Document RevitDoc { get; set; }
@@ -40,21 +46,102 @@ namespace Ark.Revit
             string modelPath = data.FilePath;
             if (String.IsNullOrWhiteSpace(modelPath)) throw new InvalidDataException(nameof(modelPath));
 
+            string modelDirectory = Path.GetDirectoryName(modelPath);
+
             Document doc = data.RevitDoc;
             if (doc == null) throw new InvalidOperationException("Could not open document.");
 
-            var collector = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StructuralColumns);
+            var supportedCategories = new HashSet<BuiltInCategory> { 
+                BuiltInCategory.OST_StructuralColumns, 
+                BuiltInCategory.OST_StructuralFraming,
+                BuiltInCategory.OST_StructuralTruss,
+                BuiltInCategory.OST_StructuralFoundation,
+                BuiltInCategory.OST_CurtainWallMullions,
+                BuiltInCategory.OST_CurtainWallPanels,
+                BuiltInCategory.OST_Rooms,
+                BuiltInCategory.OST_Areas,
+                BuiltInCategory.OST_Walls,
+                BuiltInCategory.OST_Floors,
+                BuiltInCategory.OST_Ceilings,
+                BuiltInCategory.OST_Doors,
+                BuiltInCategory.OST_Windows,
+                BuiltInCategory.OST_Stairs,
+                BuiltInCategory.OST_Ramps,
+                BuiltInCategory.OST_Roofs,
+                BuiltInCategory.OST_Furniture,
+            };
+
+            var categoriesFilter = new ElementMulticategoryFilter(supportedCategories);
+
+            var collector = new FilteredElementCollector(doc);
+            collector.WherePasses(categoriesFilter);
+
             var columns = collector.ToElements();
-            
+
+            // Init Speckle
+            SpecklePathProvider.OverrideApplicationDataPath(modelDirectory);
+            Setup.Init("ark-v1", "ark", new SpeckleLogConfiguration(Serilog.Events.LogEventLevel.Debug, true, false, false, false, false));
+
             var converter = new ConverterRevit();
             converter.SetContextDocument(doc);
             var revitDocumentAggregateCache = new RevitDocumentAggregateCache(doc);
             converter.SetContextDocument(revitDocumentAggregateCache);
 
-            var speckleObjects = converter.ConvertToSpeckle(columns[0]);
-            //var speckleObjects = MockObjects();
+            var commitBuilder = new RevitCommitObjectBuilder(CommitCollectionStrategy.ByCollection);
 
-            Console.WriteLine($"Converted {columns.Count} columns to {speckleObjects.GetTotalChildrenCount()} Speckle objects.");
+            foreach (var column in columns)
+            {
+                Console.WriteLine($"Converting column {column.Id}.");
+                Base result = converter.ConvertToSpeckle(column);
+                Console.WriteLine($"Converted column {column.Id}.");
+                commitBuilder.IncludeObject(result, column);
+                Console.WriteLine($"Included column {column.Id} in commit.");
+            }
+
+            var rootCommitObject = converter.ConvertToSpeckle(doc) ?? new Collection();
+            Console.WriteLine("Converted document.");
+
+            commitBuilder.BuildCommitObject(rootCommitObject);
+            Console.WriteLine("Built commit object.");
+
+            SendToSpeckle(rootCommitObject, modelDirectory);
+        }
+
+        private static async void SendToSpeckle(Base rootCommitObject, string blobStorageFolder = null)
+        {
+            var startSendTime = DateTime.Now;
+            Console.WriteLine("SendToSpeckle started");
+            string speckleToken = "8575964fa83e5f7b08e9da82ceb428f4c6dfb1ee8a";
+            string projectId = "f97a0b4c05";
+            string modelName = "structure";
+
+            var account = new Account()
+            {
+                token = speckleToken,
+                serverInfo = new ServerInfo()
+                {
+                    url = "https://app.speckle.systems",
+                }
+            };
+
+            var client = new Client(account);
+            var transport = new ServerTransport(account, projectId, 60, blobStorageFolder);
+
+            var objectId = await Operations.Send(rootCommitObject, new List<ITransport>() { transport });
+            Console.WriteLine($"Sent commit object to Speckle with ID {objectId}.");
+            Console.WriteLine("SendToSpeckle finished");
+
+            await client.CommitCreate(new CommitCreateInput()
+            {
+                streamId = projectId,
+                objectId = objectId,
+                branchName = "structure",
+                message = "Hello from Ark!",
+                sourceApplication = "Ark.Revit",
+                totalChildrenCount = (int)rootCommitObject.GetTotalChildrenCount()
+            });
+            Console.WriteLine($"Created commit in Speckle with ID {objectId}.");
+            Console.WriteLine($"SendToSpeckle took {DateTime.Now - startSendTime}ms.");
         }
 
         private static Base MockObjects()
