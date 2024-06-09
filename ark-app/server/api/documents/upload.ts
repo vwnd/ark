a95a4291f3c2ac1c745b3753b7f0d41655ccf5cf
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-06-09 08:50:59 +0200

    upload to s3 and oss

--- ark-app/server/api/documents/upload.ts
+++ ark-app/server/api/documents/upload.ts
@@ -1,10 +1,33 @@
+import { eq } from "drizzle-orm";
 import { db } from "~/db/drizzle";
 import { documents } from "~/db/schema";
+import { uploadFile } from "~/server/services/storage";
+import { uploadFile as uploadFileToAPS } from "~/server/services/aps";
+
+async function uploadRhino(file: File, key: string) {
+  const arrayBuffer = await file.arrayBuffer();
+  return await uploadFile(Buffer.from(arrayBuffer), key, file.type);
+}
+
+async function uploadRevit(file: File, key: string) {
+  return uploadFileToAPS(key, file);
+}
+
+async function triggerRevitJob(urn: string) {
+  // return this.apsService.revitToSpeckle(urn);
+}
+
+async function triggerRhinoJob(urn: string) {
+  // download model from s3 save on disk
+  // hit compute
+  // handle rhino
+}
 
 export default defineEventHandler(async (event) => {
   // handle upload
   // fixed projecid
   const projectId = 1;
+  const autoSync = false;
 
   const formData = await readFormData(event);
 
@@ -26,6 +49,22 @@ export default defineEventHandler(async (event) => {
       .returning()
   )[0];
 
-  console.log(document);
-  return "foo";
+  let urn;
+  if (extension === "rvt") {
+    urn = await uploadRevit(file, `${projectId}/${document.id}`);
+  } else if (extension === "3dm") {
+    urn = await uploadRhino(file, `${projectId}/${document.id}`);
+  }
+
+  if (!urn) throw new Error("Failed to upload file");
+
+  await db.update(documents).set({ urn }).where(eq(documents.id, document.id));
+
+  if (autoSync) {
+    if (extension === "rvt") {
+      triggerRevitJob(urn);
+    } else if (extension === "3dm") {
+      triggerRhinoJob(urn);
+    }
+  }
 });
