01d10dd1b3a3b76dc81bbfd6a7a6321a5a62517d
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-06-11 08:21:52 +0200

    chore: add nuxt-auth and dependencies

--- ark-app/nuxt.config.ts
+++ ark-app/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@sidebase/nuxt-auth"],
   colorMode: {
     preference: "light",
   },
