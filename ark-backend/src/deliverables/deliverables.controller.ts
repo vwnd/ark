ff31ebac5a863eb90833b4f3979ad18dc55d46b5
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-06-12 20:38:18 +0200

    chore: remove old backend

--- ark-backend/src/deliverables/deliverables.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('deliverables')
-export class DeliverablesController {}
--- ark-backend/src/deliverables/deliverables.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DeliverablesController } from './deliverables.controller';
-import { DeliverablesService } from './deliverables.service';
-
-@Module({
-  controllers: [DeliverablesController],
-  providers: [DeliverablesService]
-})
-export class DeliverablesModule {}
