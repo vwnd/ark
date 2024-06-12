488b2569e1a5e1363ecf0f5b5a098c0880b87099
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-06-12 22:48:10 +0200

    revert: fix

--- ark-app/server/api/auth/[...].ts
+++ ark-app/server/api/auth/[...].ts
@@ -48,28 +48,6 @@ export default NuxtAuthHandler({
 
       return true;
     },
-    async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
-      const isRelativeUrl = url.startsWith("/");
-      if (isRelativeUrl) {
-        return `${baseUrl}${url}`;
-      }
-
-      const isSameOriginUrl = new URL(url).origin === baseUrl;
-      const alreadyRedirected = url.includes("callbackUrl=");
-      if (isSameOriginUrl && alreadyRedirected) {
-        const originalCallbackUrl = decodeURIComponent(
-          url.split("callbackUrl=")[1]
-        );
-
-        return originalCallbackUrl;
-      }
-
-      if (isSameOriginUrl) {
-        return url;
-      }
-
-      return baseUrl;
-    },
   },
   pages: {
     signIn: "/login",
