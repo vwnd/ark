9e82659bc39639320813859b5ec80f13f4a18855
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-06-15 17:27:46 +0200

    refactor: user dropdown

--- ark-app/components/AppHeader.vue
+++ ark-app/components/AppHeader.vue
@@ -1,35 +1,6 @@
 <template>
-  <header>
-    <nav class="flex w-full items-center justify-between mb-8">
-      <BaseLogo class="w-20 h-12" />
-      <UDropdown v-if="authData?.user" :items="items">
-        <UAvatar
-          v-if="authData.user"
-          :src="authData?.user?.image || undefined"
-          :alt="authData.user.name || 'user-name'"
-          class="bg-red-400"
-          size="sm"
-          :ui="{ placeholder: 'text-white' }"
-        />
-      </UDropdown>
-    </nav>
+  <header class="flex w-full items-center justify-between mb-8">
+    <BaseLogo class="w-20 h-12" />
+    <UserDropdown />
   </header>
 </template>
-
-<script setup lang="ts">
-const { data: authData, signOut } = useAuth();
-
-const items = [
-  [
-    {
-      label: "Sign Out",
-      icon: "i-heroicons-arrow-right-start-on-rectangle",
-      click: () => {
-        signOut();
-      },
-    },
-  ],
-];
-</script>
-
-<style scoped></style>
---
+++ ark-app/components/UserDropdown.vue
@@ -0,0 +1,31 @@
+<template>
+  <UDropdown v-if="authData?.user" :items="items">
+    <UAvatar
+      v-if="authData.user"
+      :src="authData?.user?.image || undefined"
+      :alt="authData.user.name || 'user-name'"
+      class="bg-red-400"
+      size="sm"
+      :ui="{ placeholder: 'text-white' }"
+    />
+  </UDropdown>
+</template>
+
+<script setup lang="ts">
+import type { DropdownItem } from "#ui/types";
+const { data: authData, signOut } = useAuth();
+
+const items: DropdownItem[][] = [
+  [
+    {
+      label: "Sign Out",
+      icon: "i-heroicons-arrow-right-start-on-rectangle",
+      click: () => {
+        signOut();
+      },
+    },
+  ],
+];
+</script>
+
+<style scoped></style>
