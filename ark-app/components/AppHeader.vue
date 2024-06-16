d222bdf4a2754cc153abd547e2fddb0d34d0f002
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-06-16 09:55:14 +0200

    feat: added beta badge

--- ark-app/components/AppHeader.vue
+++ ark-app/components/AppHeader.vue
@@ -1,6 +1,9 @@
 <template>
   <header class="flex w-full items-center justify-between mb-8">
-    <BaseLogo class="w-20 h-12" />
+    <span class="flex items-center">
+      <BaseLogo class="w-20 h-12" />
+      <UBadge color="primary" variant="outline" class="ml-4" label="Beta" />
+    </span>
     <UserDropdown />
   </header>
 </template>
