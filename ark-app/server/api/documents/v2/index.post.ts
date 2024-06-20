c7926e3be170ec7d294023e55dc10dc62ea8bf1a
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-06-20 08:19:16 +0200

    feat: trigger job in documents v2

--- ark-app/server/api/documents/v2/index.post.ts
+++ ark-app/server/api/documents/v2/index.post.ts
@@ -1,5 +1,10 @@
 import { z, useValidatedBody } from "h3-zod";
-import { createDocument } from "~/server/lib";
+import {
+  createDocument,
+  getSignedURL,
+  revitToSpeckle,
+  rhinoToSpeckle,
+} from "~/server/lib";
 import { getServerSession } from "#auth";
 
 export default defineEventHandler(async (event) => {
@@ -21,12 +26,36 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  await createDocument({
+  const document = await createDocument({
     name,
     projectId,
     createdBy: session.uid,
     key,
   });
+  console.log("Document created", document);
+
+  if (!document.urn) return;
+
+  const signedDownloadURL = await getSignedURL(document.urn);
+
+  console.log("Triggering job", document.type);
+  if (document.type === "rvt") {
+    revitToSpeckle(signedDownloadURL);
+  } else if (document.type === "3dm") {
+    rhinoToSpeckle({
+      ark: {
+        document: document.id,
+      },
+      rhino: {
+        model: signedDownloadURL,
+      },
+      speckle: {
+        model: "ark/rhino",
+        project: "f97a0b4c05",
+        token: process.env.SPECKLE_BOT_TOKEN || "",
+      },
+    });
+  }
 
   setResponseStatus(event, 201);
 });
