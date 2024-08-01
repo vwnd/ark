3af66e337f0f1a97eb9dea576552d2ae8c787b68
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-08-01 11:36:01 -0300

    feat: open auth dialog

--- ark-app/components/SpeckleConnectPanel.vue
+++ ark-app/components/SpeckleConnectPanel.vue
@@ -1,12 +1,29 @@
 <template>
   <section class="flex-1 items-center flex bg-gray-100 rounded-lg border">
     <UContainer class="flex items-center gap-3">
-      <UButton color="blue">Connect</UButton>
-      <p class="text-blue-500 text-sm">your Speckle Account.</p>
+      <UButton color="blue" @click="handleSpeckleConnect">Get started</UButton>
+      <p class="text-blue-500 text-sm">by connecting your Speckle Account.</p>
     </UContainer>
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const appId = useRuntimeConfig().public.speckleAppId;
+const handleSpeckleConnect = () => {
+  const codeChallgenge = crypto.randomUUID();
+
+  localStorage.setItem("SPECKLE_CODE_CHALLENGE", codeChallgenge);
+
+  const speckleAuthUrl = `https://app.speckle.systems/authn/verify/${appId}/${codeChallgenge}`;
+
+  const width = 600;
+  const height = 800;
+  const left = (window.innerWidth - width) / 2;
+  const top = (window.innerHeight - height) / 2;
+  const options = `width=${width},height=${height},left=${left},top=${top}`;
+
+  window.open(speckleAuthUrl, "Ark Speckle Auth", options);
+};
+</script>
 
 <style scoped></style>
--- ark-app/nuxt.config.ts
+++ ark-app/nuxt.config.ts
@@ -14,4 +14,9 @@ export default defineNuxtConfig({
   colorMode: {
     preference: "light",
   },
+  runtimeConfig: {
+    public: {
+      speckleAppId: "",
+    },
+  },
 });
