3f4bde04e73c2078eec087cbb576f764f171f46b
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-08-01 11:20:47 -0300

    feat: move code to new component

---
+++ ark-app/components/SpeckleConnectPanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="flex-1 items-center flex bg-gray-100 rounded-lg border">
+    <UContainer class="flex items-center gap-3">
+      <UButton color="blue">Connect</UButton>
+      <p class="text-blue-500 text-sm">your Speckle Account.</p>
+    </UContainer>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- ark-app/pages/index.vue
+++ ark-app/pages/index.vue
@@ -143,11 +143,6 @@ const speckleAuth = false;
         ></iframe>
       </div>
     </main>
-    <main v-else class="flex-1 items-center flex bg-gray-100 rounded-lg border">
-      <UContainer class="flex items-center gap-3">
-        <UButton color="blue">Connect</UButton>
-        <p class="text-blue-500 text-sm">your Speckle Account.</p>
-      </UContainer>
-    </main>
+    <SpeckleConnectPanel v-else />
   </div>
 </template>
