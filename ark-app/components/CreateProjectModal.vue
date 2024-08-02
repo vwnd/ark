93e78d5474c6ba5c877d336040c2efe271d7c355
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-08-02 14:00:16 -0300

    feat: open and closes create project modal

---
+++ ark-app/components/CreateProjectModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <UModal v-model="model">
+    <UCard>
+      <template #header>
+        <div class="flex items-start justify-between">
+          <div>
+            <h2 class="text-gray-900 dark:text-white font-semibold">
+              Create project
+            </h2>
+            <p class="mt-1 text-gray-500 dark:text-gray-400 text-sm">
+              Enter the information below to create a new project.
+            </p>
+          </div>
+          <UButton
+            :padded="false"
+            color="gray"
+            variant="link"
+            icon="i-heroicons-x-mark-20-solid"
+            @click="model = false"
+          />
+        </div>
+      </template>
+
+      <Placeholder class="h-32" />
+
+      <template #footer>
+        <div class="flex flex-row-reverse gap-4">
+          <UButton>Create</UButton>
+          <UButton variant="outline" @click="model = false">Cancel</UButton>
+        </div>
+      </template>
+    </UCard>
+  </UModal>
+</template>
+
+<script setup lang="ts">
+const model = defineModel<boolean>();
+</script>
--- ark-app/components/home/HomeWelcomeCreateProjectCard.vue
+++ ark-app/components/home/HomeWelcomeCreateProjectCard.vue
@@ -2,7 +2,7 @@
   <HomeWelcomeCard
     title="Create a project"
     description="Start publishing to Speckle"
-    @click="handleCreateProject"
+    @click="isOpen = true"
   >
     <div
       class="h-fit aspect-1 bg-amber-100 rounded border-gray-400 dark:border-gray-500 opacity-75 px-4 flex items-center justify-center"
@@ -12,11 +12,10 @@
         class="h-12 w-12 text-amber-500"
       />
     </div>
+    <CreateProjectModal v-model="isOpen" />
   </HomeWelcomeCard>
 </template>
 
 <script setup lang="ts">
-const handleCreateProject = () => {
-  console.log("Create project");
-};
+const isOpen = ref(false);
 </script>
