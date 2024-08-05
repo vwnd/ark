10a429f4ae859ced25cb2e7d542bf205dce56f8e
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-08-05 14:26:18 +0200

    feat: create project and return id

--- ark-app/server/lib/speckle/create-project.ts
+++ ark-app/server/lib/speckle/create-project.ts
@@ -1,5 +1,36 @@
+import { z } from "h3-zod";
+
+const createProjectMutation = `
+  mutation CreateProject {
+    projectMutations {
+      create {
+        id
+      }
+    }
+  }
+`;
+
 export async function createProject(data: {
   accessToken: string;
 }): Promise<string> {
-  return "speckle-project-id";
+  const response = await fetch("https://app.speckle.systems/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${data.accessToken}`,
+    },
+    body: JSON.stringify({
+      query: createProjectMutation,
+    }),
+  });
+
+  const json = await response.json();
+
+  if (json.errors || !json.data || !json.data.projectMutations.create.id) {
+    throw new Error("Failed to create Speckle project");
+  }
+
+  const id = z.string().parse(json.data.projectMutations.create.id);
+
+  return id;
 }
