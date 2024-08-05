67b0590d887f6045c68ff8d643f77141ae52069d
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-08-05 11:26:30 +0200

    styles: adjust grid

--- ark-app/components/home/WelcomeSection.vue
+++ ark-app/components/home/WelcomeSection.vue
@@ -7,7 +7,7 @@
         </template>
 
         <div
-          class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-4"
+          class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4"
         >
           <HomeWelcomePlaygroundCard />
           <HomeWelcomeCreateProjectCard v-if="speckleAuth" />
