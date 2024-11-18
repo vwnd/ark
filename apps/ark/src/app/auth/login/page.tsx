426bc187745b7fb1214c446c280087a047d5c726
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-11-18 18:41:57 +0100

    feat: login button

--- apps/ark/src/app/auth/login/page.tsx
+++ apps/ark/src/app/auth/login/page.tsx
@@ -1,46 +1,20 @@
 "use client";
 
-import login from "./login";
-import { useFormState } from "react-dom";
 import { Button } from "@/components/ui/button";
 import GoogleIcon from "@/components/icons/google-icon";
 
 export default function LoginPage() {
-  const [state, formAction] = useFormState(login, { error: "" });
-
   return (
-    <form action={formAction}>
-      <div className="h-screen flex items-center justify-center flex-col gap-5">
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          className={`input input-bordered w-full max-w-xs ${
-            state?.error && "input-error"
-          }`}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          className={`input input-bordered w-full max-w-xs ${
-            state?.error && "input-error"
-          }`}
-        />
-        {state?.error}
-        <button type="submit" className="btn btn-primary">
-          Login
-        </button>
-        <div className="flex">
-          <Button
-            onClick={() =>
-              (window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/auth/signin/google`)
-            }
-          >
-            <GoogleIcon /> Login with Email
-          </Button>
-        </div>
+    <div className="h-screen flex items-center justify-center flex-col gap-5">
+      <div className="flex">
+        <Button
+          onClick={() =>
+            (window.location.href = `${process.env.NEXT_PUBLIC_API_URL}/auth/signin/google`)
+          }
+        >
+          <GoogleIcon className="fill-background" /> Login with Email
+        </Button>
       </div>
-    </form>
+    </div>
   );
 }
--- apps/ark/src/app/page.tsx
+++ apps/ark/src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header>Ark</header>
+      <header>
+        <p>Ark</p>
+        <Link href="/auth/login">Login</Link>
+      </header>
       <main className="flex flex-col items-center">
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="model-file">Model File</Label>
